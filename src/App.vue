4fe8091b9050c688b72d810d1d4d66e30b3c0239
xuwenchao <842390367@qq.com>
2018-07-02 18:06:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,149 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="GraphQl Demo" />
+    <input type="text" v-model="username" placeholder="input the username" @keydown.enter="getUser">
+    <div class="buttons">
+      <button @click="getUser">获取用户信息</button>
+      <button @click="getFollowers">获取该用户追随者</button>
+      <button @click="getRepos">获取该用户仓库</button>
+      <button>使用graphQl获取以上三者的信息</button>
+      <button @click="clear">clear</button>
+    </div>
+    <div v-if="user">
+      <h2>Profile</h2>
+      <img class="avatar" :src="user.avatar_url" alt="avatar">
+      <p>Id: {{user.id}}</p>
+      <p>Name: {{user.name}}</p>
+      <p>Location: {{user.location}}</p>
+      <p>Followers: {{user.followers}}</p>
+    </div>
+    <div class="followers" v-if="followers">
+      <h2>Followers</h2>
+      <p v-for="(item, index) in followers" :key="index">
+        <img :src="item.avatar_url">
+        <span>{{item.login}}</span>
+      </p>
+    </div>
+    <div class="repos" v-if="repos">
+      <h2>Repos</h2>
+      <p v-for="(item, index) in repos" :key="index">
+        <span>{{item.id}}</span>
+        <span>{{item.name}}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+const restUrl = 'https://api.github.com'
+export default {
+  name: 'app',
+  data() {
+    return {
+      username: 'yyx990803',
+      user: null,
+      followers: null,
+      repos: null
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    // 获取用户信息
+    async getUser() {
+      const { data } = await axios.get(`${restUrl}/users/${this.username}`)
+      this.user = data
+    },
+    // 获取追随者信息
+    async getFollowers() {
+      const { data } = await axios.get(
+        `${restUrl}/users/${this.username}/followers`
+      )
+      this.followers = data
+    },
+    // 获取仓库信息
+    async getRepos() {
+      const { data } = await axios.get(
+        `${restUrl}/users/${this.username}/repos`
+      )
+      this.repos = data
+    },
+    clear() {
+      this.user = null
+      this.followers = null
+      this.repos = null
+    }
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  margin: 0 auto;
+}
+
+.buttons {
+  margin: 40px 0;
+}
+
+button {
+  border: 1px solid #2c3e50;
+  margin: 10px;
+  font-size: small;
+  padding: 10px;
+  border-radius: 3px;
+  cursor: pointer;
+  background: #fff;
+}
+
+h2 {
+  margin: 50px 0 10px;
+  text-align: left;
+}
+
+.avatar {
+  display: inline-block;
+  height: 200px;
+  width: 200px;
+}
+input {
+  outline: none;
+  border: 1px solid #2c3e50;
+  padding: 5px 10px;
+  font-size: 18px;
+  border-radius: 3px;
+  width: 200px;
+}
+
+p {
+  width: 500px;
+}
+.followers {
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+}
+.repos {
+  p {
+    text-align: left;
+  }
+  span {
+    margin: 0 20px;
+  }
+}
+</style>
